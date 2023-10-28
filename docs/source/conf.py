36d3d62b63c9f560b318bc648ef0f6f4d82b04f9
Manuel Jesús Jiménez Navarro <59199831+manjimnav@users.noreply.github.com>
2023-10-28 18:55:01 +0200

    Update conf.py with sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.graphviz',
-    'sphinx.ext.inheritance_diagram'
+    'sphinx.ext.inheritance_diagram',
+    'sphinx_rtd_theme'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
