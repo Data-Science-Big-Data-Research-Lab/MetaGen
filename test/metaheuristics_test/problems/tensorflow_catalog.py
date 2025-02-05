18c48c8db10373c96e30fb294e76c71de57364e5
David <davgutavi@gmail.com>
2025-02-05 19:10:10 +0100

    readme updated

--- test/metaheuristics_test/problems/tensorflow_catalog.py
+++ test/metaheuristics_test/problems/tensorflow_catalog.py
@@ -14,13 +14,13 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-from locale import normalize
 
+
+from sklearn.datasets import make_regression
+from sklearn.preprocessing import normalize, StandardScaler
+from sklearn.model_selection import train_test_split
 import numpy as np
 import tensorflow as tf
-from sklearn.datasets import make_regression
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 
 from metagen.framework import Domain, Solution, BaseConnector
 
@@ -38,44 +38,49 @@ def get_nn_domain(connector = BaseConnector()) -> Domain:
     return nn_domain
 
 
+x, y = make_regression(n_samples=1000, n_features=24)
+scaler_x = StandardScaler()
+scaler_y = StandardScaler()
+
+x = scaler_x.fit_transform(x)
+y = scaler_y.fit_transform(y.reshape(-1, 1))
+
+xs_train, xs_val, ys_train, ys_val = train_test_split(x, y, test_size=0.33, random_state=42)
+
+x_train = np.reshape(xs_train, (xs_train.shape[0], xs_train.shape[1], 1))
+y_train = np.reshape(ys_train, (ys_train.shape[0], 1))
+x_val = np.reshape(xs_val, (xs_val.shape[0], xs_val.shape[1], 1))
+y_val = np.reshape(ys_val, (ys_val.shape[0], 1))
 
 
-# scaler_x = StandardScaler()
-# scaler_y = StandardScaler()
-#
-# x, y = make_regression(n_samples=1000, n_features=24)
-#
-# x, y = make_regression(n_samples=1000, n_features=24)
-# x = normalize(x)
-# xs_train, ys_train, xs_val, ys_val = train_test_split(x, y,
-#                             test_size=0.33, random_state=42)
-#
-#
-#
-#
-#
-# def build_neural_network(solution: Solution) -> tf.keras.Sequential():
-#     model = tf.keras.Sequential()
-#
-#     for i, layer in enumerate(solution["arch"]):
-#         neurons = layer["neurons"]
-#         activation = layer["activation"]
-#         dropout = layer["dropout"]
-#         rs = True
-#         if i == len(solution["arch"]):
-#             rs = False
-#         model.add(tf.keras.layers.LSTM(neurons, activation=activation, return_sequences=rs))
-#         model.add(tf.keras.layers.Dropout(dropout))
-#     model.add(tf.keras.layers.Dense(1))
-#     # Model compilation
-#     learning_rate = solution["learning_rate"]
-#     ema = solution["ema"].value
-#     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate, use_ema=ema),
-#                 loss="mean_squared_error", metrics=[tf.keras.metrics.MAPE])
-#     return model
-#
-# def nn_fitness(solution: Solution) -> float:
-#     model = build_neural_network(solution)
-#     model.fit(x_train, y_train, epochs=10, batch_size=1024)
-#     mape = model.evaluate(x_val, y_val)[1]
-#     return mape
+def build_neural_network(solution: Solution) -> tf.keras.Sequential():
+    # Architecture building
+    model = tf.keras.Sequential()
+    for i, layer in enumerate(solution["arch"]):
+        neurons = layer["neurons"]
+        activation = layer["activation"]
+        dropout = layer["dropout"]
+        rs = True
+        if i == len(solution["arch"]):
+            rs = False
+        model.add(tf.keras.layers.LSTM(neurons,
+                                activation=activation,
+                                return_sequences=rs))
+        model.add(tf.keras.layers.Dropout(dropout))
+    model.add(tf.keras.layers.Dense(1, activation="tanh"))
+    # Model compilation
+    learning_rate = solution["learning_rate"]
+    ema = solution["ema"]
+    model.compile(optimizer=
+                    tf.keras.optimizers.Adam(
+                            learning_rate=learning_rate,
+                            use_ema=ema),
+                  loss="mean_squared_error",
+                  metrics=[tf.keras.metrics.MAPE])
+    return model
+
+def nn_fitness(solution: Solution) -> float:
+    model = build_neural_network(solution)
+    model.fit(x_train, y_train, epochs=10, batch_size=1024)
+    mape = model.evaluate(x_val, y_val)[1]
+    return mape
