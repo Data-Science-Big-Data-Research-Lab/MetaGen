56503080b8b6db994cb45d3e81bdc76c1e188470
David <davgutavi@gmail.com>
2025-02-05 18:21:45 +0100

    readme updated

--- test/metaheuristics_test/problems/tensorflow_catalog.py
+++ test/metaheuristics_test/problems/tensorflow_catalog.py
@@ -14,60 +14,68 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+from locale import normalize
+
 import numpy as np
 import tensorflow as tf
 from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import normalize
+from sklearn.preprocessing import StandardScaler
 
-from metagen.framework import Domain, Solution
+from metagen.framework import Domain, Solution, BaseConnector
 
-lstm_domain = Domain()
-lstm_domain.define_real("learning_rate", 0.0, 0.000001)
-lstm_domain.define_categorical("ema", [True, False])
-lstm_domain.define_dynamic_structure("arch", 2, 10)
-lstm_domain.define_group("layer")
-lstm_domain.define_integer_in_group("layer", "neurons", 25, 300)
-lstm_domain.define_categorical_in_group("layer", "activation", ["relu", "sigmoid", "softmax", "tanh"])
-lstm_domain.define_real_in_group("layer", "dropout", 0.0, 0.45)
-lstm_domain.set_structure_to_variable("arch", "layer")
 
-x, y = make_regression(n_samples=100, n_features=24)
+def get_nn_domain(connector = BaseConnector()) -> Domain:
+    nn_domain = Domain(connector)
+    nn_domain.define_real("learning_rate", 0.0, 0.000001)
+    nn_domain.define_categorical("ema", [True, False])
+    nn_domain.define_dynamic_structure("arch", 2, 10)
+    nn_domain.define_group("layer")
+    nn_domain.define_integer_in_group("layer", "neurons", 25, 300)
+    nn_domain.define_categorical_in_group("layer", "activation", ["relu", "sigmoid", "softmax", "tanh"])
+    nn_domain.define_real_in_group("layer", "dropout", 0.0, 0.45)
+    nn_domain.set_structure_to_variable("arch", "layer")
+    return nn_domain
+
+
+
+
+scaler_x = StandardScaler()
+scaler_y = StandardScaler()
+
+x, y = make_regression(n_samples=1000, n_features=24)
+
+x, y = make_regression(n_samples=1000, n_features=24)
 x = normalize(x)
-xs_tr, xs_ts, ys_tr, ys_ts = train_test_split(x, y, test_size=0.33, random_state=42)
-x_tr = np.reshape(xs_tr, (xs_tr.shape[0], xs_tr.shape[1], 1))
-y_tr = np.reshape(ys_tr, (ys_tr.shape[0], 1))
-x_ts = np.reshape(xs_ts, (xs_ts.shape[0], xs_ts.shape[1], 1))
-y_ts = np.reshape(ys_ts, (ys_ts.shape[0], 1))
-
-
-def lstm_fitness(solution: Solution) -> float:
-
-    model = build_neural_network(solution)
-
-    model.fit(x_tr, y_tr, epochs=10, batch_size=1024)
-
-    mape = model.evaluate(x_ts, y_ts)[1]
-
-    return mape
+xs_train, ys_train, xs_val, ys_val = train_test_split(x, y,
+                            test_size=0.33, random_state=42)
 
 
 
-def build_neural_network(solution:Solution) -> tf.keras.Sequential():
+
+
+def build_neural_network(solution: Solution) -> tf.keras.Sequential():
     model = tf.keras.Sequential()
-    arch_size = solution.get_vector_size("arch")
-    for i in range(0, arch_size):
-        neurons = solution.get_layer_component_element("arch", i, "neurons")
-        activation = solution.get_layer_component_element("arch", i, "activation")
-        dropout = solution.get_layer_component_element("arch", i, "dropout")
+
+    for i, layer in enumerate(solution["arch"]):
+        neurons = layer["neurons"]
+        activation = layer["activation"]
+        dropout = layer["dropout"]
         rs = True
-        if i == arch_size - 1:
+        if i == len(solution["arch"]):
             rs = False
         model.add(tf.keras.layers.LSTM(neurons, activation=activation, return_sequences=rs))
         model.add(tf.keras.layers.Dropout(dropout))
-    model.add(tf.keras.layers.Dense(1, activation="tanh"))
-    learning_rate = solution.get_basic_value("learning_rate")
-    ema = solution.get_basic_value("ema")
+    model.add(tf.keras.layers.Dense(1))
+    # Model compilation
+    learning_rate = solution["learning_rate"]
+    ema = solution["ema"].value
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate, use_ema=ema),
-                  loss="mean_squared_error", metrics=[tf.keras.metrics.MAPE])
+                loss="mean_squared_error", metrics=[tf.keras.metrics.MAPE])
     return model
+
+def nn_fitness(solution: Solution) -> float:
+    model = build_neural_network(solution)
+    model.fit(x_train, y_train, epochs=10, batch_size=1024)
+    mape = model.evaluate(x_val, y_val)[1]
+    return mape
